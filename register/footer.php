8231cadf76aac49387c699245b676ccea4c229ac
Godfrey Ikeozahu OJERHEGHAN <63786414+EngrGodfrey@users.noreply.github.com>
2023-08-08 00:01:26 +0100

    Add files via upload

---
+++ register/footer.php
@@ -0,0 +1,12 @@
+
+<div class="footer">
+	<hr/>
+&copy2023. Developed by A1 Computers: A1.
+<br/>
+Online Voting System.
+<br/>
+CITN Ilorin Branch
+<br/>
+Contact: <a href="tel:+2348033555883">+234 803 3555883</a> | Email: <a href="mailto:a1computersenterprisesa@gmail.com">a1computersenterprises@gmail.com</a>
+
+</div>
