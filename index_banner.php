6227b932d4e62d007cad18e2b66f1a7dddecafd8
Godfrey Ikeozahu OJERHEGHAN <63786414+EngrGodfrey@users.noreply.github.com>
2023-08-08 12:36:26 +0100

    Update index_banner.php

--- index_banner.php
+++ index_banner.php
@@ -2,7 +2,7 @@
 <div class="side-bar" 
             style="background-color:rgba(0, 100, 0, 1); font-size:25px; text-align:center; margin-top:-10px; padding-top:20px; padding-bottom:0px;">
 
-                <a href="#" style="color:white; font-weight:bold; font-size:30px;">University Online Voting System.</a>
+                <a href="#" style="color:white; font-weight:bold; font-size:30px;">CITN Ilorin $ District Society e-Voting System.</a>
                 </a>
 
         <nav class="nav-menue">
